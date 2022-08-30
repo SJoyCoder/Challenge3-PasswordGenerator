8b55e9936fb48caace531b5052e3111be63c9938
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-08-30 15:17:09 -0500

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("something happened")
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
