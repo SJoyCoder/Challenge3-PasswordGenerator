cf7231b0c748313f43279e167ec78ae8d923d089
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-08-30 21:00:39 -0500

    added array

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,10 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+const num = [0,1,2,3,4,5,6,7,8,9];
+const sym = ["!", "@", "#", "$", "%", "^", "&", "*", "/", "<", ">", "?", "/"];
+const lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "X"]
 
 function generatePassword() {
   console.log("something happened")
