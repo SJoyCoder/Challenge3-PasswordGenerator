30ecf512800b38bc18dc2b842cdc25e4578add8b
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-08-31 20:59:27 -0500

    added lowercase letters and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,15 @@
 // Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 const num = [0,1,2,3,4,5,6,7,8,9];
 const sym = ["!", "@", "#", "$", "%", "^", "&", "*", "/", "<", ">", "?", "/"];
 const lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "X"]
+const uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-function generatePassword() {
-  console.log("something happened")
-  let passwordLength = prompt("Password Length?");
-  if (passwordLength <= 128){
-    //continue
-  }else{
-    //try again
-  }
-  let lowercase = prompt("Lowercase Letters?");
-  if (lowercase == true){
-    //include lowercase
-  }else{
-    //don't include lowercase
-  }
-  let uppercase = prompt("Uppercase Letters?");
-  if (uppercase == true){
-    //include uppercase
-  }else{
-    //don't include uppercase
-  }
-  let numbers = prompt("Numbers?");
-  if (numbers == true){
-    //include numbers
-  }else{
-    //don't include numbers
-  }
-  let specCharac = prompt("Special Characters?");
-  if (specCharac == true){
-    //include special characters
-  }else{
-    //don't
-  }
- 
-  return "password"
-}
-//promt for length, lowercase, uppercase, num, spec
-//validate and @ least one character
-//passwor generated correctly
-//should be in alert or written to page
+// Get references to the #generate element
+let lengthInput = window.prompt("Please enter length of password desired.")
+let includeNum = window.confirm("Include numbers?")
+
 
 // Write password to the #password input
 function writePassword() {
@@ -56,6 +19,29 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword(passwordLength, lowercase, uppercase, numbers, specCharac) {
+  
+let password = "";
+
+for (let i = 0; i < lengthInput; i++) {
+  let randomNum = Math.floor(Math.random()*lowercaseLetters.length);
+  let randomLetter = lowercaseLetters[randomNum];
+  password += randomLetter;
+}
+
+
+
+return password;
+}
+
+
+//promt for length, lowercase, uppercase, num, spec
+
+//validate and @ least one character
+//passwor generated correctly
+//should be in alert or written to page
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
