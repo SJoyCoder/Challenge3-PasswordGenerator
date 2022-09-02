8a0ca91cc5c4f69afe9ac61fc42829b84dba2bf1
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-02 15:57:47 -0500

    added password variable and fixed bugs

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,12 @@
 // Assignment code here
 
 var generateBtn = document.querySelector("#generate");
-const startingArray = [];
-const lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-const sym = ["!", "@", "#", "$", "%", "^", "&", "*", "/", "<", ">", "?", "/"];
-const num = [0,1,2,3,4,5,6,7,8,9];
+var startingArray = [];
+var finalPassword = document.querySelector("#password");
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var sym = ["!", "@", "#", "$", "%", "^", "&", "*", "/", "<", ">", "?", "/"];
+var num = [0,1,2,3,4,5,6,7,8,9];
 
 
 
@@ -34,6 +35,7 @@ function generatePassword(passwordLength, includeLowercase, includeUppercase, in
   let password = "";
   
   var passwordLength = prompt("Please enter length of password desired.")
+  passwordLength= parseInt(passwordLength)
     if (passwordLength >= 128){
       alert("Password must be less than 128 characters"); 
       prompt("Please enter password length less than 128 characters.");
@@ -41,39 +43,40 @@ function generatePassword(passwordLength, includeLowercase, includeUppercase, in
 
   var includeLowercase = confirm("Do you want to include lowercase?")
     if(includeLowercase === true){
-      startingArray.concat(lowercaseLetters);
+     startingArray += startingArray.concat(lowercaseLetters);
 
-      startingArray;
     }
   
   
 var includeUppercase = confirm("Do you want to include uppercase letters?")
   if(includeUppercase === true){
-    startingArray.concat(uppercaseLetters);
+    startingArray += startingArray.concat(uppercaseLetters);
   }
 
 var includeSym = confirm("Do you want to include symbols?")
   if (includeSym === true){
-    startingArray.concat(sym);
+   startingArray += startingArray.concat(sym);
 }
 
 var includeNum = confirm("Do you want to include numbers?")
   if (includeNum === true){
-    startingArray.concat(num);
+   startingArray += startingArray.concat(num);
 
   }
+
+
   
   for (let i = 0; i < passwordLength; i++) {
-  let randomNumber = Math.floor(Math.random()*sym.length);
-  let randomNum = includeNum[randomNumber];
+  let randomNumber = Math.floor(Math.random()*startingArray.length);
+  let randomNum = startingArray[randomNumber];
   password += randomNum;
 }
     
 return password;
 }
-
-
-//promt for length, lowercase, uppercase, num, spec
+console.log(finalPassword); 
+finalPassword.innerHTML.password;
+//promt for length, lowercase, uppercase, num, spec √
 
 //validate and @ least one character
 //passwor generated correctly
